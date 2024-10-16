ed3f41d563e284d10637ddb934060a2ccc2d989b
Vasyl-Bab1 <hen23xar@gmail.com>
2024-10-16 15:56:01 +0300

    15:55 16.10.2024

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)    
 
 @app.route('/')
 def main():
-    return 'Hello, world!'
+    return '<h1>Hello, world!</h1>', 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+
+    return "This is your homepage :) "
+
+if __name__ == "__main__":
+    app.run()  # Launch built-in web server and run this Flask webapp, debug=True
+ 
