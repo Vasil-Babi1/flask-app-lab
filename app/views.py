5376309f600edd14da1c93ef29e2f7eb26cea1f7
Vasyl-Bab1 <hen23xar@gmail.com>
2024-10-22 10:33:55 +0300

    Кінець Лекції 22.10.2024

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render_template,abort
+from flask import request, redirect, url_for, render_template, abort
 from . import app
 
 @app.route('/')
@@ -12,7 +12,9 @@ def home():
 
     return render_template("home.html", agent=agent)
 
-@app.route("/hi/<string:name>")   #/hi/Vasyl?age
+#users
+
+@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
     name = name.upper()
     age = request.args.get("age", None, int)   
@@ -25,20 +27,3 @@ def admin():
     to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
-
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-] 
-
-@app.route('/posts') 
-def get_posts():
-    return render_template("posts.html", posts=posts)
-
-@app.route('/post/<int:id>') 
-def detail_post(id):
-    if id > 3:
-        abort(404)
-    post = posts[id-1]
-    return render_template("detail_post.html", post=post)
