48f7ca7d5214137063ded7a09a5038c073a7f338
Vasyl-Bab1 <hen23xar@gmail.com>
2024-10-28 20:37:56 +0200

    Лабораторна робота 4

---
+++ app/users/views.py
@@ -0,0 +1,16 @@
+from flask import request, render_template, redirect, url_for
+from . import user_bp
+
+@user_bp.route("/hi/<string:name>") 
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", None, int)
+
+    return render_template("hi.html",
+                            name=name, age=age)
+
+@user_bp.route("/admin")
+def admin():
+    to_url = url_for("users.greetings", name="administrator", age=45, _external=True) 
+    print(to_url)
+    return redirect(to_url)
