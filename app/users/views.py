b1f29a865b4114290d27e15998ebfc4c2ae17912
Vasyl-Bab1 <hen23xar@gmail.com>
2024-10-30 17:22:54 +0200

    add coockie 30.10.2024 лекція

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
-from flask import request, render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for, make_response
 from . import user_bp
+from datetime import timedelta, datetime
 
 @user_bp.route("/hi/<string:name>") 
 def greetings(name):
@@ -14,3 +15,23 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", age=45, _external=True) 
     print(to_url)
     return redirect(to_url)
+
+
+
+@bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', '', max_age=timedelta(seconds=60))
+    return response
+
+@bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
