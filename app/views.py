b7d48a35f0fd8df8eec70c4ffff3715f6a057f48
Vasyl-Bab1 <hen23xar@gmail.com>
2024-11-19 11:02:51 +0200

    Лабораторна 6 додаткова частина

--- app/views.py
+++ app/views.py
@@ -1,11 +1,10 @@
-from flask import request, redirect, url_for, render_template, abort
-from . import app
+from flask import request, redirect, url_for, render_template, current_app
 
-@app.route('/')
+@current_app.route('/')
 def main():
     return render_template("base.html")
 
-@app.route('/homepage') 
+@current_app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
@@ -14,7 +13,7 @@ def home():
 
 #users
 
-@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
+@current_app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
     name = name.upper()
     age = request.args.get("age", None, int)   
@@ -22,16 +21,16 @@ def greetings(name):
     return render_template("hi.html", 
                            name=name, age=age)
 
-@app.route("/admin")
+@current_app.route("/admin")
 def admin():
     to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
 
-@app.route("/resume")
+@current_app.route("/resume")
 def resume():
     return render_template("resume.html", title="Резюме студента Бабійчука Василя")
 
-@app.errorhandler(404)
+@current_app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
